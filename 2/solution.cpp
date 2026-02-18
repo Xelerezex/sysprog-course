ddddc9a2bb2d6bf23cc074d2cfe2ac05577ebffa
xelerezex <xelerezex@gmail.com>
2026-02-18 11:30:50 +0300

    [lab-2]: Created built in change directory.

--- 2/solution.cpp
+++ 2/solution.cpp
@@ -14,20 +14,82 @@
 
 namespace utils {
 
+constexpr int success = 0;
+constexpr int failure = 1;
+constexpr int error_code = -1;
+
+int builtInChangeDirectory(const std::vector<std::string> &arguments) {
+    int last_status = success;
+    // handling HOME
+    if (arguments.empty()) {
+        const auto home_path_raw = std::getenv("HOME");
+        if (home_path_raw == nullptr) {
+            std::cerr << "bash: cd: HOME not set\n";
+            return failure;
+        }
+        const std::string home_path_variable(home_path_raw);
+        if (home_path_variable.empty()) {
+            std::cerr << "bash: cd: HOME not set\n";
+            return failure;
+        }
+        // change directory of process
+        if (chdir(home_path_variable.c_str()) != error_code) {
+            last_status = success;
+        } else {
+            last_status = failure;
+            std::cerr << "bash: cd: " << home_path_variable << ": " << strerror(errno) << "\n";
+        }
+    } else if (arguments.size() == 1) {
+        const std::string &path_variable = arguments.at(0);
+        if (path_variable.empty()) {
+            std::cerr << "bash: cd: No such file or directory\n";
+            return failure;
+        }
+        // change directory of process
+        if (chdir(path_variable.c_str()) != error_code) {
+            last_status = success;
+        } else {
+            last_status = failure;
+            std::cerr << "bash: cd: " << path_variable << ": " << strerror(errno) << "\n";
+        }
+    } else {
+        last_status = failure;
+        std::cerr << "bash: cd: too many arguments\n";
+    }
+
+    return last_status;
+}
+
 int statusToBash(const int status) {
-    int result = 0;
+    int result = success;
     if (WIFEXITED(status)) {
         result = WEXITSTATUS(status);
     } else if (WIFSIGNALED(status)) {
         result = 128 + WTERMSIG(status);
     } else {
-        result = 1;
+        result = failure;
     }
     return result;
 }
 
+int waitForChild(const pid_t child_pid) {
+    int status;
+    while (true) {
+        const pid_t wait_pid = waitpid(child_pid, &status, 0);
+        if (wait_pid == error_code && errno == EINTR) {
+            errno = success;
+            continue;
+        }
+        if (wait_pid == error_code && errno != EINTR) {
+            return failure;
+        }
+        break;
+    }
+    return statusToBash(status);
+}
+
 int executeRegularNonBackgroundCommand(command &command) {
-    int last_status = 0;
+    int last_status = success;
     std::vector<char *> arguments;
     arguments.reserve(command.args.size() + 2);
 
@@ -41,34 +103,30 @@ int executeRegularNonBackgroundCommand(command &command) {
     // End for arguments:
     arguments.push_back(nullptr);
 
-    if (const pid_t child_pid = fork(); child_pid == -1) {
-        std::cerr << "bash: " << arguments.at(0) << ": child fork error\n";
-        last_status = 1;
-    } else if (child_pid > 0) {
-        int status;
-        do {
-            const pid_t wait_pid = waitpid(child_pid, &status, 0);
-            if (wait_pid == -1 && errno == EINTR) {
-                errno = 0;
-                continue;
-            }
-            if (wait_pid == -1 && errno != EINTR) {
-                last_status = 1;
-                return last_status;
-            }
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        last_status = statusToBash(status);
-        // parent
-    } else if (child_pid == 0) {
-        const auto execute_result = execvp(arguments.at(0), arguments.data());
-        if (execute_result == -1) {
-            if (errno == ENOENT) {
-                std::cerr << "bash: " << arguments.at(0) << ": command not found\n";
-                _exit(127);
-            }
-            std::cerr << "bash: " << arguments.at(0) << ": " << strerror(errno) << "\n";
-            _exit(126);
+    // if change directory -> do not fork()
+    if (command.exe == "cd") {
+        return builtInChangeDirectory(command.args);
+    }
+
+    // fork process
+    const pid_t child_pid = fork();
+    if (child_pid <= error_code) {
+        std::cerr << "bash: fork: " << strerror(errno) << "\n";
+        return failure;
+    }
+    if (child_pid > 0) {
+        // parent:
+        return waitForChild(child_pid);
+    }
+
+    // child: (child_pid == 0)
+    if (const auto execute_result = execvp(arguments.at(0), arguments.data()); execute_result == error_code) {
+        if (errno == ENOENT) {
+            std::cerr << "bash: " << arguments.at(0) << ": command not found\n";
+            _exit(127);
         }
+        std::cerr << "bash: " << arguments.at(0) << ": " << strerror(errno) << "\n";
+        _exit(126);
     }
 
     return last_status;
@@ -77,20 +135,28 @@ int executeRegularNonBackgroundCommand(command &command) {
 int executeCommandLine(command_line *line) {
     assert(line != nullptr);
 
+    int last_status = success;
+
+    // dumb check: execute only one command from now
+    if (line->exprs.size() != 1 || line->out_type != OUTPUT_TYPE_STDOUT || line->is_background) {
+        return last_status;
+    }
+    for (const auto &[type, command] : line->exprs) {
+        if (type != EXPR_TYPE_COMMAND) {
+            return last_status;
+        }
+    }
+
+    // main logic
     if (line->out_type == OUTPUT_TYPE_STDOUT) {
         if (!line->is_background) {
             // printf("Is background: %d\n", static_cast<int>(line->is_background));
         }
-
     } else if (line->out_type == OUTPUT_TYPE_FILE_NEW) {
-        // printf("new file - \"%s\"\n", line->out_file.c_str());
     } else if (line->out_type == OUTPUT_TYPE_FILE_APPEND) {
-        // printf("append file - \"%s\"\n", line->out_file.c_str());
     } else {
         assert(false);
     }
-
-    int last_status = 0;
 
     // printf("Expressions:\n");
     for (auto &[type, command] : line->exprs) {
@@ -103,10 +169,12 @@ int executeCommandLine(command_line *line) {
                 last_status = executeRegularNonBackgroundCommand(command.value());
             }
 
-        } /*else if (type == EXPR_TYPE_PIPE) {
+        } else if (type == EXPR_TYPE_PIPE) {
         } else if (type == EXPR_TYPE_AND) {
         } else if (type == EXPR_TYPE_OR) {
-        } else { assert(false); } */
+        } else {
+            assert(false);
+        }
     }
     return last_status;
 }
@@ -146,13 +214,13 @@ int createAndExecuteCommand(parser *parser_object, const std::string &line) {
     parser_feed(parser_object, line.data(), static_cast<uint32_t>(line.size()));
     command_line *current_line_raw = nullptr;
     while (true) {
-        const parser_error error_code = parser_pop_next(parser_object, &current_line_raw);
-        if (error_code == PARSER_ERR_NONE && current_line_raw == nullptr) {
+        const parser_error parser_error_code = parser_pop_next(parser_object, &current_line_raw);
+        if (parser_error_code == PARSER_ERR_NONE && current_line_raw == nullptr) {
             break;
         }
         std::unique_ptr<command_line> current_line_owner(current_line_raw);
 
-        if (error_code != PARSER_ERR_NONE) {
+        if (parser_error_code != PARSER_ERR_NONE) {
             continue;
         }
         last_status = executeCommandLine(current_line_raw);
